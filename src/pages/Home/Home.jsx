99f587b3bf9a1af6eb99cd05a4f7ed6969a2f914
mizan-chowdhury <mizan1034@gmail.com>
2024-05-31 22:06:16 +0600

    scroll feature added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Banner from "../../Components/Banner/Banner";
 import Projects from "../Projects/Projects";
+import "./app.scss";
 
 const Home = () => {
   return (
-    <div>
-      <Banner></Banner>
-      <Projects />
+    <div className="home-container">
+      <div className="scroll-div">
+        <Banner />
+      </div>
+      <div className="scroll-div-2">
+        <Projects />
+      </div>
     </div>
   );
 };
